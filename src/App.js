104ccdf84f021eff1c235ca35a7640a0e89aae52
adejbakhsh <adejbakh@student.42.fr>
2020-05-19 21:42:33 +0200

    Proto searchbar I am getting the result in movies but not displaying them yet adding package react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 import './components/layouts/Layout.js'
 import Layout from './components/layouts/Layout.js';
+import Searchbar from './components/Searchbar.js';
+import {
+  BrowserRouter,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-          <Layout></Layout>
+      <BrowserRouter>
+      <Layout></Layout>
+      <Route exact path="/" component={Searchbar} />
+
+      </BrowserRouter>
     </div>
   );
 }
