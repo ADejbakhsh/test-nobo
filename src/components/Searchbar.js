b91f4325755cbc1e0da637400948d845e5bf2f2d
adejbakhsh <adejbakh@student.42.fr>
2020-05-19 14:34:43 +0200

    Layout set this include Header and Footer

---
+++ src/components/Searchbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+
+  export default function Searchbar() {
+    const classes = useStyles();
+  
+    return (
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
+        </div>
+      </form>
+    );
+  }
+  
---
+++ src/components/layouts/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar.js';
+import Footer from './Footer.js';
+
+function Layout() {
+  return (
+    <div className="Layout">
+          <Navbar></Navbar>
+          <Footer></Footer>
+    </div>
+  );
+}
+
+export default Layout;
