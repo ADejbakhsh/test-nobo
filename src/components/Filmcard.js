9bc78556771d2febde31a7da88680ecc0dd6e101
adejbakhsh <adejbakh@student.42.fr>
2020-05-20 17:54:11 +0200

    Proto movie card card need css for max size and desktop view

--- src/components/Filmcard.js
+++ src/components/Filmcard.js
@@ -1,11 +1,45 @@
-import React, {Component} from 'react';
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 
-class Filmcard extends Component {
+class Filmcard extends React.Component {
     render() {
-    let filmJson = this.props.movies;
+        const cards = [];
+        if (this.props.movies) {
+            this.props.movies.forEach(element => {
+                let name = element["show"]["name"];
+                let img;
+                if (element["show"]["image"])
+                    img = element["show"]["image"]["medium"];
+                else
+                    img = "https://www.labaleine.fr/sites/default/files/image-not-found.jpg";
+                cards.push(
+                    <Card>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                image={img}
+                                title={name}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {name}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                );
 
-    return <h6>{filmJson[0].score.toString() }</h6>
+            });
+        }
+        return (cards);
+
     }
 }
+
+
 export default Filmcard;
